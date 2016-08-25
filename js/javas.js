f42ba158b128a35e1db97a79abceda2c1f39902c
byOPKY <orrillokarol@gmail.com>
2016-08-25 15:17:46 -0500

    subiendo ejercicios relaizados

---
+++ js/javas.js
@@ -0,0 +1,13 @@
+window.alert("Calcula tu Sueldo Semanal");
+var num1 =parseInt(prompt ("¿Cuantas horas trabajas a la semana?"));
+var num2 =parseInt(prompt ("¿Cuanto te pagan por hora?"));
+var suma = num1 * num2;
+document.write("Tu sueldo deberia ser S/. " + suma);
+
+
+
+
+
+
+
+
